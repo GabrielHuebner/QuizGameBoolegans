77caf7e0e0a6041334a7f2afbd1acfc2d6c3ce89
Leon Freudenthaler <leon.freudenthaler@stud.fh-campuswien.ac.at>
2020-12-23 20:49:44 +0100

    Path fix

--- src/main/java/at/ac/fhcampuswien/boolegans/App.java
+++ src/main/java/at/ac/fhcampuswien/boolegans/App.java
@@ -2,11 +2,9 @@ package at.ac.fhcampuswien.boolegans;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-import java.net.URL;
 
 public class App extends Application {
 
@@ -16,11 +14,13 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(new URL("file:///C:\\Users\\Gabriel\\IdeaProjects\\QuizGameBoolegans\\src\\main\\java\\at\\ac\\fhcampuswien\\boolegans\\layout.fxml"));
-        Pane pane = loader.load();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("layout.fxml"));  // only looks into /resources folder for files!
 
-        Scene scene = new Scene(pane);
+        //URL url = new File("src/main/java/at/ac/fhcampuswien/boolegans/layout.fxml").toURI().toURL(); // another way to load fxml file - but not recommended
+        //Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root, 300, 275);
+
+        //Scene scene = new Scene(pane);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
